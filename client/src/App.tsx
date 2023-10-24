6e21e9e155840ebdc20b664186086fbd34ed7c67
saywhat <95282021+loveflora@users.noreply.github.com>
2023-10-24 22:06:08 +0900

    feat: 로그인, 회원가입 틀

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,6 +10,7 @@ import Header from './components/common/Header';
 import NotFound from './pages/NotFound';
 import Management from './pages/Management';
 import Login from './pages/user/Login';
+import Join from './pages/user/Join';
 
 function App() {
   return (
@@ -18,9 +19,11 @@ function App() {
         <Header />
         <Routes>
           <Route path='/' element={<Intro />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/join' element={<Join />} />
           <Route path='/main' element={<Main />} />
           <Route path='/todo' element={<Todo />} />
-          <Route path='/group' element={<GroupHome />} />
+          <Route path='/group/*' element={<GroupHome />} />
           <Route path='/myPage' element={<MyPage />} />
           <Route path='/management' element={<Management />} />
           {/* 404 처리는 제일 밑에 있어야 함 */}
--- client/src/pages/user/Login.tsx
+++ client/src/pages/user/Login.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <div>
+      <h1>Login</h1>
+        <Link to="/join">
+          <button>SSO 로그인</button>
+        </Link>
+    </div>
+  );
 }
