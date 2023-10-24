146a123492a8058452325f7baa43f3ec8beae491
lemon4974 <139740955+lemon4974@users.noreply.github.com>
2023-10-24 21:28:38 +0900

    feat: Todo add, check 기능

--- client/src/components/todo/TodoList.tsx
+++ client/src/components/todo/TodoList.tsx
@@ -1,13 +1,56 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
+import { ToDoItem } from '../../types/types';
 import TodoItem from './TodoItem';
 
 export default function TodoList() {
+  const [todos, setTodos] = useState<ToDoItem[]>([]);
+  const [newTodo, setNewTodo] = useState<string>('');
+  const checked = {
+    textDecoration: 'line-through',
+  };
+
+  const addTodo = () => {
+    const updatedTodos = [
+      ...todos,
+      { id: Date.now(), text: newTodo, completed: false },
+    ];
+    setTodos(updatedTodos);
+    setNewTodo('');
+  };
+
+  const toggleComplete = (targetId: number) => {
+    const updatedTodos = todos.map((todo) => {
+      return todo.id === targetId
+        ? { ...todo, completed: !todo.completed }
+        : todo;
+    });
+    setTodos(updatedTodos);
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      addTodo();
+    }
+  };
+
   return (
     <div>
       <h1>TodoList </h1>
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
+      <div>
+        <input
+          type="text"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+          onKeyDown={handleKeyDown}
+          placeholder="새로운 투두를 추가해보세요."
+        />
+        <button onClick={addTodo}>ADD</button>
+      </div>
+      <ul>
+        {todos.map((todo) => (
+          <TodoItem key={todo.id} todo={todo} toggleComplete={toggleComplete} />
+        ))}
+      </ul>
     </div>
   );
 }
