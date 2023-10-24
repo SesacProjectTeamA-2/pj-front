0a751e3da86ab702e125076179c47644fa59bdde
Bean <86273626+ch0rckbean@users.noreply.github.com>
2023-10-25 01:05:07 +0900

    feat : nickname edit 컴포넌트  추가

--- client/src/components/common/Header.tsx
+++ client/src/components/common/Header.tsx
@@ -2,35 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Header() {
-<<<<<<< Updated upstream
-  return (
-    <div>
-      <Link to="/">
-        <h1>로고</h1>
-      </Link>
-      <nav>
-        <Link to="/todo">
-          <button>Todo</button>
-        </Link>
-        <Link to="/group">
-          <button>Group</button>
-        </Link>
-        <Link to="/myPage">
-          <button>My</button>
-        </Link>
-        <Link to="/management">
-          {/* 관리자만 보이는 버튼 */}
-          <button>Management</button>
-        </Link>
-        <img
-          src="/asset/images/user.png"
-          style={{ width: '40px', height: '40px' }}
-          alt="userImg"
-        ></img>
-      </nav>
-    </div>
-  );
-=======
+
     return (
         <div>
             <Link to="/">
@@ -66,12 +38,11 @@ export default function Header() {
                     ></img>
                 </Link>
                 <img
-                    src="/asset/icons/chat.png"
+                    src="/asset/icons/chat.svg"
                     style={{ width: '40px', height: '40px' }}
                     alt="chatImg"
                 />
             </nav>
         </div>
     );
->>>>>>> Stashed changes
 }
--- client/src/components/myPage/Nickname.tsx
+++ client/src/components/myPage/Nickname.tsx
@@ -1,15 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 export default function Nickname(): JSX.Element {
-    const [nickname, setNickname] = useState<String>('');
+    const [nickname, setNickname] = useState<string | number>('달려라하니');
+    const [readOnlyVal, setReadOnlyVal] = useState<boolean>(true);
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    // edit btn 눌렀을 때 focus + 수정 가능 상태로 바뀜
+    const changeReadOnly = (): void => {
+        inputRef.current?.focus();
+        setReadOnlyVal(!readOnlyVal);
+    };
+
     return (
         <div>
             <input
-                readOnly={false}
+                readOnly={readOnlyVal}
                 onChange={(e) => setNickname(e.target.value)}
-              
+                value={nickname}
+                ref={inputRef}
             />
-            <img src="/asset/icons/edit.png" alt="editImg"></img>
+            <button onClick={(e) => changeReadOnly()}>
+                <img src="/asset/icons/edit.svg" alt="editImg"></img>
+            </button>
         </div>
     );
 }
