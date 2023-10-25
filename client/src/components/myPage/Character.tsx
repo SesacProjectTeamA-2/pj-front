da25f84334a640b43dff390389c9ebb4e3d634e3
Bean <86273626+ch0rckbean@users.noreply.github.com>
2023-10-25 14:01:53 +0900

    fix: 클릭 css 처리 => js 이벤트로 변경 및 추가

--- client/src/components/myPage/Character.tsx
+++ client/src/components/myPage/Character.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
+import React, { MouseEventHandler } from 'react';
 
 export default function Character() {
-    // const selectedBtn: any = document.querySelector('button');
-    // const selectCharacter = (): void => {
-    //     selectedBtn.style.border = '2px solid blue';
-    //     console.log('cl');
-    // };
+    const selectedBtn: any = document.querySelector('button');
+    const selectCharacter = (e: React.MouseEvent<HTMLElement>): void => {
+        selectedBtn.style.border = '2px solid blue';
+        console.log(e.target);
+    };
     return (
         <>
             <div className="character-div">
-                <button className="selectBtn">
+                <button
+                    className="selectBtn"
+                    onClick={(e: React.MouseEvent<HTMLElement>) =>
+                        selectCharacter(e)
+                    }
+                >
                     <img src="/asset/images/rabbit2.png" alt="img1" />
                 </button>
-                <button className="selectBtn">
+                <button
+                    className="selectBtn"
+                    onClick={(e: React.MouseEvent<HTMLElement>) =>
+                        selectCharacter(e)
+                    }
+                >
                     <img src="/asset/images/dog2.png" alt="img2" />
                 </button>
-                <button className="selectBtn">
+                <button
+                    className="selectBtn"
+                    onClick={(e: React.MouseEvent<HTMLElement>) =>
+                        selectCharacter(e)
+                    }
+                >
                     <img src="/asset/images/cat2.svg" alt="img3" />
                 </button>
-                <button className="selectBtn">
+                <button
+                    className="selectBtn"
+                    onClick={(e: React.MouseEvent<HTMLElement>) =>
+                        selectCharacter(e)
+                    }
+                >
                     <img src="/asset/images/sqr2.svg" alt="img4" />
                 </button>
             </div>
--- client/src/pages/user/MyPage.tsx
+++ client/src/pages/user/MyPage.tsx
@@ -9,7 +9,7 @@ export default function MyPage() {
     return (
         <div>
             로그인 안 했을 때: 로그인 버튼 보임 + 채팅 버튼 안 보임 <br></br>
-            로그인 했을 때: 로그인 버튼 안 보임 + 채팅 버튼 보임
+            로그인 했을 때: 로그인 버튼 안 보임 + 채팅 버튼/프로필 보임
             <br></br>ㄴ 관리자일 때: Management 버튼 추가로 보임
             <Nickname />
             <Introduce />
