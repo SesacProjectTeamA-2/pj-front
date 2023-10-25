e3c2f642122ceb40f021679181872a3c1a6879f0
saywhat <95282021+loveflora@users.noreply.github.com>
2023-10-25 21:28:04 +0900

    feat: group 틀 생성

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,7 @@ import './styles/scss/base/reset.scss';
 import Intro from './pages/Intro';
 import Main from './pages/Main';
 import Todo from './pages/Todo';
-import GroupHome from './pages/group/GroupHome';
+import GroupHome from './pages/group/Group';
 import MyPage from './pages/user/MyPage';
 import Header from './components/common/Header';
 import NotFound from './pages/NotFound';
@@ -13,25 +13,25 @@ import Login from './pages/user/Login';
 import Join from './pages/user/Join';
 
 function App() {
-  return (
-    <div className='App'>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path='/' element={<Intro />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/join' element={<Join />} />
-          <Route path='/main' element={<Main />} />
-          <Route path='/todo' element={<Todo />} />
-          <Route path='/group/*' element={<GroupHome />} />
-          <Route path='/myPage' element={<MyPage />} />
-          <Route path='/management' element={<Management />} />
-          {/* 404 처리는 제일 밑에 있어야 함 */}
-          <Route path='*' element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Intro />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/join" element={<Join />} />
+                    <Route path="/main" element={<Main />} />
+                    <Route path="/todo" element={<Todo />} />
+                    <Route path="/group/*" element={<GroupHome />} />
+                    <Route path="/myPage" element={<MyPage />} />
+                    <Route path="/management" element={<Management />} />
+                    {/* 404 처리는 제일 밑에 있어야 함 */}
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
--- client/src/pages/group/GroupHome.tsx
+++ client/src/pages/group/GroupHome.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
-export default function GroupHome() {
-  return <div>GroupHome</div>;
+export default function GroupDetail() {
+    return <div>GroupDetail</div>;
 }
