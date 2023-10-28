2a30804c160d5e6bf9871dd177a2a76b2f364858
Bean <86273626+ch0rckbean@users.noreply.github.com>
2023-10-28 15:29:10 +0900

    fix: Basic, Group Layout 수정

--- client/src/pages/user/MyPage.tsx
+++ client/src/pages/user/MyPage.tsx
@@ -16,37 +16,50 @@ export default function MyPage() {
     return (
         <div className="section">
             {/* 로그인 안 했을 때: 로그인 버튼 보임 + 채팅 버튼 안 보임 <br></br>
-            로그인 했을 때: 로그인 버튼 안 보임 + 채팅 버튼/프로필 보임
-            <br></br>ㄴ 관리자일 때: Management 버튼 추가로 보임 */}
-            <ProfilePic />
-            <Nickname />
-            <Introduce />
+                로그인 했을 때: 로그인 버튼 안 보임 + 채팅 버튼/프로필 보임
+                <br></br>ㄴ 관리자일 때: Management 버튼 추가로 보임 */}
+            <div className="myPage-div-one">
+                <ProfilePic />
+                <Nickname />
+                <Introduce />
+            </div>
+
+            <div className="myPage-div-two">
+                <br></br>
+                <h3>내 캐릭터</h3>
+                <CharacterList />
+            </div>
 
             <br></br>
-            <h3>내 캐릭터</h3>
-            <CharacterList />
-
-            <br></br>
-            <h3>관심분야</h3>
-            <InterestedList />
-
-            <br></br>
-            <h3>명언</h3>
-            <Phrase />
+            <div className="myPage-div-three">
+                <h3>관심분야</h3>
+                <InterestedList />
+                <br></br>
+                <h3>명언</h3>
+                <Phrase />
+            </div>
 
             <br></br>
             <hr></hr>
-            <h3>메인화면 설정</h3>
-            <SetMainList />
-            <br></br>
-            <PsnCoverImg />
+            <div className="myPage-div-four">
+                <h3>메인화면 설정</h3>
+                <SetMainList />
+                <br></br>
+                <PsnCoverImg />
+            </div>
+
             <br></br>
             <hr></hr>
-            <Quit />
+            <div className="myPage-div-five">
+                <Quit />
+            </div>
             <br></br>
-            <button id="myPage-edit-btn" type="submit">
-                수정 완료
-            </button>
+
+            <div className="myPage-div-six">
+                <button id="myPage-edit-btn" type="submit">
+                    수정 완료
+                </button>
+            </div>
         </div>
     );
 }
