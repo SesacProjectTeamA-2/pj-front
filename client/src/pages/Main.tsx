026ad5b2f7d2c63f53fa193a01fa0571fb6ad7d7
lemon4974 <139740955+lemon4974@users.noreply.github.com>
2023-10-28 17:40:19 +0900

    style: 메인페이지 배치

--- client/src/pages/Main.tsx
+++ client/src/pages/Main.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import MainImg from '../components/main/MainImg';
+import Content from '../components/main/Content';
 
 export default function Main() {
     return (
         <div className="section">
             <MainImg />
+            <Content />
         </div>
     );
 }
