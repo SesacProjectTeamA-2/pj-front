40e0e9e613df9f1912b2a4980339ddff961e5f2a
Bean <86273626+ch0rckbean@users.noreply.github.com>
2023-10-29 16:14:31 +0900

    feat: tmp

--- client/src/pages/user/Login.tsx
+++ client/src/pages/user/Login.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 
 import '../../styles/scss/pages/user/login.scss';
-import { Link } from 'react-router-dom';
 
 export default function Login() {
     const onClick = async () => {
@@ -16,11 +16,19 @@ export default function Login() {
     return (
         <div className="section">
             <h1>안녕하세요!</h1>
-            <button onClick={onClick}>구글</button>
-            <button>네이버</button>
-            <button>카카오</button>
+            {/* <div className="google-btn-div">
+            </div>
+            <div className="kakao-btn-div">
+            </div> 
+            <div className="naver-btn-div">
+            </div> */}
+            <img src="/asset/images/googleLogin-img.png" alt="" className="a" />
+            <button className="login-btn" id="google-btn"></button>
+            {/* <img src="/asset/images/kakaoLogin-img.png" alt="" className="a" />
+            <img src="/asset/images/naverLogin-img.png" alt="" className="a" /> */}
+
             <button>
-                <Link to="/join">회원 가입</Link>
+                <Link to="/join">회원이 아니신가요?</Link>
             </button>
         </div>
     );
