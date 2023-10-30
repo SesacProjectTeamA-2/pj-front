327bc7cd2dc8535495c8413eee3d817fd40c6958
Bean <86273626+ch0rckbean@users.noreply.github.com>
2023-10-30 18:39:17 +0900

    refactor: bootstrap => MUI & showFooter()

--- client/src/components/common/layout/oldGroupLayout.tsx
+++ client/src/components/common/layout/oldGroupLayout.tsx
@@ -1,69 +1,69 @@
-import React from 'react';
-import SidebarChat from '../SidebarChat';
+// import React from 'react';
+// import SidebarChat from '../SidebarChat';
 
-// import Container from 'react-bootstrap/Container';
-// import Row from 'react-bootstrap/Row';
-// import Col from 'react-bootstrap/Col';
-import SideBarGroup from '../SidebarGroup';
+// // import Container from 'react-bootstrap/Container';
+// // import Row from 'react-bootstrap/Row';
+// // import Col from 'react-bootstrap/Col';
+// import SideBarGroup from '../SidebarGroup';
 
-// groupbar section chat
-export default function GroupLayout({ children, showChat }: any) {
-    // console.log('showChat', showChat);
-    return (
-        // 전체 레이아웃 컨테이너
-        <div className="layout-container">
-            <Container fluid>
-                <Row>
-                    {showChat ? (
-                        <>
-                            {/* 그룹 메뉴 바 컴포넌트 들어갈 곳 */}
-                            <Col md={2} sm={2} xs={2} className="groupMenu-div">
-                                {/* [추후] 모임장, 멤버 사이드바 다름 */}
-                                <SideBarGroup />
-                            </Col>
-                            {/* 컨텐츠 컴포넌트 들어갈 곳 */}
-                            <Col
-                                md={8}
-                                sm={10}
-                                xs={10}
-                                className="section-wrapper"
-                            >
-                                {children}
-                            </Col>
-                            {/* 채팅 컴포넌트 들어갈 곳 */}
-                            <Col
-                                md={2}
-                                sm={12}
-                                xs={12}
-                                className="chatting-div"
-                            >
-                                <SidebarChat />
-                            </Col>
-                        </>
-                    ) : (
-                        <>
-                            <Col md={2} sm={2} xs={2} className="groupMenu-div">
-                                {/* [추후] 모임장, 멤버 사이드바 다름 */}
-                                <SideBarGroup />
-                            </Col>
-                            <Col
-                                md={8}
-                                sm={10}
-                                xs={10}
-                                className="section-wrapper"
-                            >
-                                {children}
-                            </Col>
-                            <Col
-                                md={2}
-                                sm={12}
-                                xs={12}
-                                className="chatting-div"
-                            ></Col>
-                        </>
-                    )}
-                </Row>
-            </Container>
-        </div>
-    );
-}
+// // groupbar section chat
+// export default function GroupLayout({ children, showChat }: any) {
+//     // console.log('showChat', showChat);
+//     return (
+//         // 전체 레이아웃 컨테이너
+//         <div className="layout-container">
+//             <Container fluid>
+//                 <Row>
+//                     {showChat ? (
+//                         <>
+//                             {/* 그룹 메뉴 바 컴포넌트 들어갈 곳 */}
+//                             <Col md={2} sm={2} xs={2} className="groupMenu-div">
+//                                 {/* [추후] 모임장, 멤버 사이드바 다름 */}
+//                                 <SideBarGroup />
+//                             </Col>
+//                             {/* 컨텐츠 컴포넌트 들어갈 곳 */}
+//                             <Col
+//                                 md={8}
+//                                 sm={10}
+//                                 xs={10}
+//                                 className="section-wrapper"
+//                             >
+//                                 {children}
+//                             </Col>
+//                             {/* 채팅 컴포넌트 들어갈 곳 */}
+//                             <Col
+//                                 md={2}
+//                                 sm={12}
+//                                 xs={12}
+//                                 className="chatting-div"
+//                             >
+//                                 <SidebarChat />
+//                             </Col>
+//                         </>
+//                     ) : (
+//                         <>
+//                             <Col md={2} sm={2} xs={2} className="groupMenu-div">
+//                                 {/* [추후] 모임장, 멤버 사이드바 다름 */}
+//                                 <SideBarGroup />
+//                             </Col>
+//                             <Col
+//                                 md={8}
+//                                 sm={10}
+//                                 xs={10}
+//                                 className="section-wrapper"
+//                             >
+//                                 {children}
+//                             </Col>
+//                             <Col
+//                                 md={2}
+//                                 sm={12}
+//                                 xs={12}
+//                                 className="chatting-div"
+//                             ></Col>
+//                         </>
+//                     )}
+//                 </Row>
+//             </Container>
+//         </div>
+//     );
+// }
