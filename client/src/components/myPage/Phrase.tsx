6bc898c56d4ebd5a610acc1a1c68d86d57b16deb
Bean <86273626+ch0rckbean@users.noreply.github.com>
2023-11-01 17:54:55 +0900

    fix: doing

--- client/src/components/myPage/Phrase.tsx
+++ client/src/components/myPage/Phrase.tsx
@@ -7,31 +7,34 @@ export default function Phrase(): JSX.Element {
     const [readOnlyVal, setReadOnlyVal] = useState<boolean>(true);
     const inputRef = useRef<HTMLTextAreaElement>(null);
 
+    // 버튼 클릭 상태 관리
+    const [recSelected, setRecSelected] = useState<boolean>(false);
+    const [selfSelected, setSelfSelected] = useState<boolean>(false);
+
     // edit btn 눌렀을 때 focus + 수정 가능 상태로 바뀜
     const changeReadOnly = (): void => {
         inputRef.current?.focus();
         setReadOnlyVal(!readOnlyVal);
     };
 
-    //버튼 선택 시 : 배경 핑크, 글씨 하양 변경
-    // 추천 선택 시 : 텍스트area 비활성화
-    // 내가 선택 시 : 활성화 + 보임
-    // 각 버튼 한개만 선택되어야
-    const phraseSelect = (e: React.MouseEvent<HTMLElement>): void => {
+    const phraseSelect = (e: React.ChangeEvent<HTMLElement>): void => {
         const phraseModeBtn: HTMLElement = e.target as HTMLElement;
         const phraseModeBtnVal: string | null =
-            phraseModeBtn.getAttribute('for');
+            phraseModeBtn.getAttribute('value');
 
         switch (phraseModeBtnVal) {
-            case 'phraseMode-btn1':
+            case 'recommend':
                 setReadOnlyVal(true);
-                phraseModeBtn.style.backgroundColor = '#ED8D8D';
-                phraseModeBtn.style.color = 'white';
+                setRecSelected((prev) => !prev);
+                console.log(phraseModeBtn.parentNode);
+                console.log(recSelected);
                 break;
-            case 'phraseMode-btn2':
-                setReadOnlyVal(false);
 
+            case 'self':
+                setReadOnlyVal(false);
+                setSelfSelected((prev) => !prev);
                 break;
+
             default:
                 return;
         }
@@ -66,26 +69,29 @@ export default function Phrase(): JSX.Element {
             <div className="phraseMode-btn-div">
                 <label
                     className="phrase-label"
-                    onClick={(e) => phraseSelect(e)}
                     id="phraseMode-btn1"
+                    style={{
+                        background: recSelected ? '5px solid #ed8d8d' : 'gray',
+                        color: 'white',
+                    }}
                 >
                     <input
                         className="phrase-radio"
                         type="radio"
                         name="phraseMode"
+                        value="recommend"
+                        onChange={(e) => phraseSelect(e)}
                     ></input>
                     추천해주세요
                 </label>
 
-                <label
-                    className="phrase-label"
-                    onClick={(e) => phraseSelect(e)}
-                    id="phraseMode-btn2"
-                >
+                <label className="phrase-label" id="phraseMode-btn2">
                     <input
                         className="phrase-radio"
                         type="radio"
                         name="phraseMode"
+                        value="self"
+                        onChange={(e) => phraseSelect(e)}
                     ></input>
                     내가 적을래요
                 </label>
--- client/src/pages/user/MyPage.tsx
+++ client/src/pages/user/MyPage.tsx
@@ -32,13 +32,13 @@ export default function MyPage() {
 
             <div className="myPage-div-two">
                 <br></br>
-                <h3 className="myPage-h3">내 캐릭터</h3>
+                <h3 className="myPage-p">내 캐릭터</h3>
                 <CharacterList />
             </div>
 
             <div className="myPage-div-three">
                 <div className="myPage-div-three-one">
-                    <h3 className="myPage-h3">관심분야</h3>
+                    <h3 className="myPage-p">관심분야</h3>
                     <p>최대 3개</p>
                     <p>{warningInfo}</p>
                     <InterestedList
@@ -47,7 +47,7 @@ export default function MyPage() {
                     />
                 </div>
                 <div className="myPage-div-three-two">
-                    <h3 className="myPage-h3">명언</h3>
+                    <h3 className="myPage-p">명언</h3>
                     <Phrase />
                 </div>
             </div>
@@ -61,7 +61,7 @@ export default function MyPage() {
 
             <div className="myPage-div-five">
                 <div className="myPage-div-five-one">
-                    <h3 className="myPage-h3">회원탈퇴</h3>
+                    <h3 className="myPage-p">회원탈퇴</h3>
                 </div>
                 <div className="myPage-div-five-two">
                     <Quit />
