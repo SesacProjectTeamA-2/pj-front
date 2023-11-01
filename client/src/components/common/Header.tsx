723f1181defb2145d51720f9289fa71ce14bfeda
Bean <86273626+ch0rckbean@users.noreply.github.com>
2023-11-01 18:08:13 +0900

    fix: logo svg.ver 반영 및 size 수정

--- client/src/components/common/Header.tsx
+++ client/src/components/common/Header.tsx
@@ -12,7 +12,7 @@ export default function Header(props: any) {
                     <div className="logo-container">
                         {/* <div className="logo-text">MOTI</div> */}
                         <img
-                            src="/asset/1.png"
+                            src="/asset/logo1.svg"
                             className="logo-img"
                             alt="logo"
                         />
