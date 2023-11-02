7d81735e13ab41c90d395e67162b1d9e80e38261
lemon4974 <139740955+lemon4974@users.noreply.github.com>
2023-11-02 18:45:37 +0900

    feat: 명언 API & CSS

--- client/src/components/main/Quotes.tsx
+++ client/src/components/main/Quotes.tsx
@@ -1,26 +1,41 @@
 import React from 'react';
+import { kadvice } from 'kadvice';
 
 export default function Quotes() {
-    return (
-        <div className="content-grid-box quotes-div-flex">
-            <div className="quotes">
-                <span>"</span>
-                삶의 가장 큰 영예는 넘어지지 않는 것이 아니라, 우리가 넘어질
-                때마다 다시 일어나는데에 있다.
-                <span>"</span>
-                <div className="quotes-author-flex">
-                    <div> - 랄프 왈도 에머슨</div>
-                </div>
-                {/* <div className="quotes-btn-flex">
-                    <button>랜덤 생성</button>
+    const advice = kadvice.getOne();
 
-                    <img
-                        src="/asset/icons/edit.svg"
-                        alt="명언 편집 아이콘"
-                        className="upload-img-icon"
-                    />
-                </div> */}
-            </div>
+    console.log(advice);
+    return (
+        // <div className="content-grid-box quotes-div-flex">
+        //     <div className="quotes ">
+        //         <p>{advice.message}</p>
+
+        //         {/* <div className="quotes-author-flex">
+        //             <div> - {advice.author}</div>
+        //         </div> */}
+        //         <cite>
+        //             <div> - {advice.author}</div>
+        //         </cite>
+
+        //         {/* <div className="quotes-btn-flex">
+        //             <button>랜덤 생성</button>
+
+        //             <img
+        //                 src="/asset/icons/edit.svg"
+        //                 alt="명언 편집 아이콘"
+        //                 className="upload-img-icon"
+        //             />
+        //         </div> */}
+        //     </div>
+        // </div>
+        <div className="content-grid-box sample2">
+            <blockquote>
+                <br />
+
+                <p>{advice.message}</p>
+                <cite>{advice.author}</cite>
+                <br />
+            </blockquote>
         </div>
     );
 }
