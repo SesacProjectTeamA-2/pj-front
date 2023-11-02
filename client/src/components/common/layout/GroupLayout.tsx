209a886584f756bdd06c721a60e2ae294a51ae5b
Bean <86273626+ch0rckbean@users.noreply.github.com>
2023-11-03 00:55:43 +0900

    refactor : Basic & Group Layout

--- client/src/components/common/layout/GroupLayout.tsx
+++ client/src/components/common/layout/GroupLayout.tsx
@@ -15,107 +15,52 @@ export default function GroupLayout({ children, showChat }: any) {
             {/* 전체 레이아웃 컨테이너 */}
             <div className="layout-container">
                 <Grid container>
-                    {showChat ? (
-                        <>
-                            {/* 그룹 메뉴 바 컴포넌트 들어갈 곳 */}
-                            <Grid
-                                md={2}
-                                sm={2}
-                                xs={2}
-                                className="groupMenu-div"
+                    <>
+                        {/* 그룹 메뉴 바 컴포넌트 들어갈 곳 */}
+                        <Grid md={2} sm={2} xs={2} className="groupMenu-div">
+                            <Item
+                                style={{
+                                    paddingLeft: 0,
+                                    paddingRight: 0,
+                                    paddingTop: 0,
+                                }}
                             >
-                                <Item
-                                    style={{
-                                        paddingLeft: 0,
-                                        paddingRight: 0,
-                                        paddingTop: 0,
-                                    }}
-                                >
-                                    <SideBarGroup />
-                                </Item>
-                            </Grid>
+                                <SideBarGroup />
+                            </Item>
+                        </Grid>
 
-                            <Grid
-                                md={8}
-                                sm={10}
-                                xs={10}
-                                className="section-wrapper"
+                        <Grid
+                            md={8}
+                            sm={10}
+                            xs={10}
+                            className="section-wrapper"
+                        >
+                            {' '}
+                            <Item
+                                style={{
+                                    paddingLeft: 0,
+                                    paddingRight: 0,
+                                    paddingTop: 0,
+                                }}
                             >
-                                {' '}
-                                <Item
-                                    style={{
-                                        paddingLeft: 0,
-                                        paddingRight: 0,
-                                        paddingTop: 0,
-                                    }}
-                                >
-                                    {children}
-                                </Item>
-                            </Grid>
+                                {children}
+                            </Item>
+                        </Grid>
 
-                            {/* 채팅 컴포넌트 들어갈 곳 */}
-                            <Grid
-                                md={2}
-                                sm={12}
-                                xs={12}
-                                className="chatting-div"
+                        {/* 채팅 컴포넌트 들어갈 곳 */}
+                        <Grid md={2} sm={12} xs={12} className="chatting-div">
+                            <Item
+                                style={{
+                                    paddingLeft: 0,
+                                    paddingRight: 0,
+                                    paddingTop: 0,
+                                }}
                             >
-                                <Item
-                                    style={{
-                                        paddingLeft: 0,
-                                        paddingRight: 0,
-                                        paddingTop: 0,
-                                    }}
-                                >
-                                    <SidebarChat />
-                                </Item>
-                            </Grid>
-                            <Footer />
-                        </>
-                    ) : (
-                        <>
-                            <Grid
-                                md={2}
-                                sm={2}
-                                xs={2}
-                                className="groupMenu-div"
-                            >
-                                <Item
-                                    style={{
-                                        paddingLeft: 0,
-                                        paddingRight: 0,
-                                        paddingTop: 0,
-                                    }}
-                                >
-                                    <SideBarGroup />{' '}
-                                </Item>
-                            </Grid>
-                            <Grid
-                                md={8}
-                                sm={10}
-                                xs={10}
-                                className="section-wrapper"
-                            >
-                                <Item
-                                    style={{
-                                        paddingLeft: 0,
-                                        paddingRight: 0,
-                                        paddingTop: 0,
-                                        justifyContent: 'center',
-                                    }}
-                                >
-                                    {' '}
-                                    {children}
-                                </Item>
-                            </Grid>
-                            <Grid
-                                md={2}
-                                sm={12}
-                                xs={12}
-                                className="chatting-div"
-                            ></Grid>
-                        </>
-                    )}
+                                {showChat ? <SidebarChat /> : null}
+                            </Item>
+                        </Grid>
+                        <Footer />
+                    </>
                 </Grid>
                 <Footer />
             </div>
