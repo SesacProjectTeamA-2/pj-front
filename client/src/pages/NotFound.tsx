5a9403c804cd34083a920c73804f4906d977bedc
Bean <86273626+ch0rckbean@users.noreply.github.com>
2023-11-02 15:38:58 +0900

    feat: 404

--- client/src/pages/NotFound.tsx
+++ client/src/pages/NotFound.tsx
@@ -1,17 +1,27 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 import '../styles/scss/pages/notFound.scss';
-
-import ErrMsg from '../components/common/NotFound/ErrMsg';
-import R3F from '../components/common/NotFound/R3F';
-import ErrBtn from '../components/common/NotFound/ErrBtn';
 
 export default function NotFound() {
     return (
         <div className="section err-section">
-            <ErrMsg />
-            <R3F />
-            <ErrBtn />
+            <div className="errMsg-div">
+                <span>4</span>
+                <span>0</span>
+                <span>4</span>
+            </div>
+            <div className="errImg-div">
+                <img
+                    src="/asset/images/errRabbit.png"
+                    alt="errRabbit"
+                    id="errRabbit-img"
+                />
+            </div>
+            <div className="errBtn-div">
+                <Link to="/">
+                    <button id="err-btn">RETURN</button>
+                </Link>
+            </div>
         </div>
     );
 }
