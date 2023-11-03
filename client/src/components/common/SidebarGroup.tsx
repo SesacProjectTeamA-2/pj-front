57fe873ec1735f600be8ce6fcea09f02c979c0e3
lemon4974 <139740955+lemon4974@users.noreply.github.com>
2023-11-03 13:01:25 +0900

    feat: 그룹페이지 메뉴바 드롭다운

--- client/src/components/common/SidebarGroup.tsx
+++ client/src/components/common/SidebarGroup.tsx
@@ -24,36 +24,48 @@ export default function SideBarGroup() {
                     <Link to="/group/home/1">
                         <li className="title4-hover-bigger">홈</li>
                     </Link>
-                    <li>게시판</li>
-
-                    <ul className="title5">
-                        <Link to="/group/noti/1">
-                            <li className="title5-hover-bigger">공지사항</li>
-                        </Link>
-                        <Link to="/group/board/1">
-                            <li className="title5-hover-bigger">자유</li>
-                        </Link>
-                    </ul>
-                    <li>미션</li>
-                    <ul className="title5">
-                        <li>진행 중</li>
-                        <ul className="title6">
-                            <Link to="/group/mission/1/1">
-                                <li className="title6-hover-bigger">
-                                    알고리즘
+                    {/* 게시판 */}
+                    <li className="dropdown-noti">
+                        <button className="dropbtn-noti">게시판</button>
+                        <ul className="title5 dropdown-content-noti">
+                            <Link to="/group/noti/1">
+                                <li className="title5-hover-bigger">
+                                    공지사항
                                 </li>
                             </Link>
-                            <Link to="/group/mission/1/2">
-                                <li className="title6-hover-bigger">블로깅</li>
-                            </Link>
-                            <Link to="/group/mission/1/3">
-                                <li className="title6-hover-bigger">모각코</li>
+                            <Link to="/group/board/1">
+                                <li className="title5-hover-bigger">자유</li>
                             </Link>
                         </ul>
-                        <Link to="/group/mission/done/1">
-                            <li className="title5-hover-bigger">완료</li>
-                        </Link>
-                    </ul>
+                    </li>
+                    {/* 미션 */}
+                    <li className="dropdown">
+                        <button className="dropbtn">미션</button>
+
+                        <ul className="title5 dropdown-content">
+                            <li>진행 중</li>
+                            <ul className="title6">
+                                <Link to="/group/mission/1/1">
+                                    <li className="title6-hover-bigger">
+                                        알고리즘
+                                    </li>
+                                </Link>
+                                <Link to="/group/mission/1/2">
+                                    <li className="title6-hover-bigger">
+                                        블로깅
+                                    </li>
+                                </Link>
+                                <Link to="/group/mission/1/3">
+                                    <li className="title6-hover-bigger">
+                                        모각코
+                                    </li>
+                                </Link>
+                            </ul>
+                            <Link to="/group/mission/done/1">
+                                <li className="title5-hover-bigger">완료</li>
+                            </Link>
+                        </ul>
+                    </li>
                 </ul>
             </div>
 
