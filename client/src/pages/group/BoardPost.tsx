066ce8bceb0a5317a5830983e7f7a395d04622f7
saywhat <95282021+loveflora@users.noreply.github.com>
2023-11-07 17:07:12 +0900

    feat: 누적 랭킹 데이터 요청

--- client/src/pages/group/BoardPost.tsx
+++ client/src/pages/group/BoardPost.tsx
@@ -27,21 +27,34 @@ export default function BoardPost() {
 
     const [missionList, setMissionList] = useState<Mission[]>();
 
+    const getGroup = async () => {
+        const res = await axios.get(
+            `${process.env.REACT_APP_DB_HOST}/group/detail/${gSeq}`,
+            {
+                headers: {
+                    Authorization: `Bearer ${uToken}`,
+                },
+            }
+        );
+
+        setMissionList(res.data.groupMission);
+    };
+
+    // const getGroupMission = async () => {
+    //     const res = await axios
+    //         .get(`${process.env.REACT_APP_DB_HOST}/mission/group/${gSeq}`, {
+    //             headers: {
+    //                 Authorization: `Bearer ${uToken}`,
+    //             },
+    //         })
+    //         .then((res) => {
+    //             setMissionList(res.data.groupMission);
+    //         });
+    // };
+
     useEffect(() => {
-        const getGroup = async () => {
-            const res = await axios.get(
-                `${process.env.REACT_APP_DB_HOST}/group/detail/${gSeq}`,
-                {
-                    headers: {
-                        Authorization: `Bearer ${uToken}`,
-                    },
-                }
-            );
-
-            setMissionList(res.data.groupMission);
-        };
-
         getGroup();
+        // getGroupMission();
     }, []);
 
     console.log('>>>>>>', missionList);
@@ -60,7 +73,7 @@ export default function BoardPost() {
         gbTitle: '',
         gbContent: '',
         gbCategory: 'notice',
-        mSeq: '',
+        mSeq: 1,
     });
 
     const [selected, setSelected] = useState<string>('');
@@ -90,7 +103,15 @@ export default function BoardPost() {
             gbCategory: selectedValue,
         });
 
-        // console.log('Selected:', e.target.value);
+        if (selectedValue !== '') {
+            setBoard({
+                ...board,
+                gbCategory: 'mission',
+                mSeq: Number(selectedValue),
+            });
+        }
+
+        console.log('Selected:', e.target.value);
         // console.log('setSelected 후', board);
     };
 
@@ -123,6 +144,8 @@ export default function BoardPost() {
 
     console.log(board);
 
+    // console.log('oooooo', missionList);
+
     return (
         <div className="section section-group">
             {/* title 값 넘겨 받기 ! */}
@@ -138,19 +161,13 @@ export default function BoardPost() {
                             <option value="notice">공지사항</option>
                             <option value="free">자유/질문</option>
 
-                            {missionList?.map(
-                                (mission: MissionType, idx: number) => {
-                                    return (
-                                        <>
-                                            <option value={`mission${idx}`}>
-                                                {mission.mTitle}
-                                            </option>
-                                            {/* <option value="mission2">미션2</option> */}
-                                            {/* <option value="mission3">미션3</option> */}
-                                        </>
-                                    );
-                                }
-                            )}
+                            {missionList?.map((mission: any, idx: number) => {
+                                return (
+                                    <option value={mission.mSeq} key={idx}>
+                                        {mission.mTitle}
+                                    </option>
+                                );
+                            })}
                         </select>
                     </div>
 
