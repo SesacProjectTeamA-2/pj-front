201e192938d16c6c62dfd2643373691f08215131
Bean <86273626+ch0rckbean@users.noreply.github.com>
2023-11-15 15:30:28 +0900

    fix: logo & 회원가입 카테고리 에러

--- client/src/pages/user/MyPage.tsx
+++ client/src/pages/user/MyPage.tsx
@@ -12,8 +12,8 @@ import InterestedList from '../../components/common/InterestedList';
 import Phrase from '../../components/myPage/Phrase';
 import ProfilePic from '../../components/myPage/ProfilePic';
 import Quit from '../../components/myPage/Quit';
-// import SetMainList from '../../components/myPage/SetMainList';
-// import PsnCoverImg from '../../components/myPage/PsnCoverImg';
+import SetMainList from '../../components/myPage/SetMainList';
+import PsnCoverImg from '../../components/myPage/PsnCoverImg';
 
 export default function MyPage() {
     const cookie = new Cookies();
@@ -53,11 +53,10 @@ export default function MyPage() {
                 }
                 setInput(nickname);
                 setContent(coverLetter);
-                // if (category1 && category2 && category3) {
-                // 선택 안 하면 undefined 들어있어서 선택이 안 됨
-                //값 있을 때만 실행하도록 조건문 넣었음
-                setSelectedArr([category1, category2, category3]);
-                // }
+                if (category1 && category2 && category3) {
+                    // 선택 안 하면 null 값 들어있어서 값 있을 때만 실행하도록 조건문 넣었음
+                    setSelectedArr([category1, category2, category3]);
+                }
                 setSelectedCharacter(character);
 
                 setPhraseCtt(phrase);
@@ -76,11 +75,9 @@ export default function MyPage() {
 
     const handlerChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const formData = new FormData(); // 사진 담을 formData 객체 생성
-        // console.log('e.target.files ', e.target.files);
 
         if (e.target.files && e.target.files[0]) {
             formData.append('image', e.target.files[0]);
-
             sendImg(formData);
         }
     };
@@ -103,7 +100,7 @@ export default function MyPage() {
                 )
                 .then((res) => {
                     console.log('post', res.data);
-                    getUserData(); // 바로 수정된 프로필 사진으로 동기화
+                    getUserData(); // 이거 해야 바로 수정된 프로필 사진으로 동기화 : 하지만 저장되지 않은 다른 값들은 초기화 돼서 옴 ㅜ
                 });
         } catch (err) {
             console.log(err);
@@ -125,14 +122,10 @@ export default function MyPage() {
     };
 
     // 4. 관심사 배열
-    // 선택 값 배열
     const [selectedArr, setSelectedArr] = useState<Array<string>>([]);
-
-    // 여기선 값 잘 반영 됨
-    // useEffect(() => {
-    //     console.log(selectedArr);
-    //     console.log(selectedArr.length);
-    // }, [selectedArr]);
+    useEffect(() => {
+        console.log(selectedArr);
+    }, [selectedArr]);
 
     // 5. 선택한 dDay id
     const [dDayPin, setDdayPin] = useState<number>(0);
@@ -165,6 +158,7 @@ export default function MyPage() {
     };
 
     useEffect(() => {
+        console.log('phraseModeBtnVal', phraseModeBtnVal, phraseCtt);
         if (phraseModeBtnVal === 'recommend') {
             // 추천 모드일 때 빈 값을 보냄
             setPhraseCtt(null);
@@ -190,6 +184,7 @@ export default function MyPage() {
         uMainGroup: number;
     }
     const patchedUserData: patchedUserDataItf = {
+        // uImg:userImgSrc,
         uName: input,
         uDesc: content,
         uPhrase: phraseCtt,
@@ -212,18 +207,20 @@ export default function MyPage() {
             await axios
                 .patch(
                     `${process.env.REACT_APP_DB_HOST}/user/mypage`,
+                    // 'http://localhost:8888/api/user/mypage',
 
                     patchedUserData,
                     {
                         headers: {
                             Authorization: `Bearer ${uToken}`,
+                            // 'Content-Type': 'multipart/form-data',
                         },
                     }
                 )
                 .then((res) => {
-                    toast.success(res.data.message, {
-                        duration: 2000,
-                    });
+                    console.log('patched', res.data.message);
+                    toast.success(res.data.message);
+                    console.log('patchedData2', patchedUserData);
                 });
         } catch (err) {
             console.log(err);
@@ -231,7 +228,6 @@ export default function MyPage() {
     };
 
     // 3. 회원 탈퇴
-    // DELETE 요청 함수 작성 필요 + Quit에 prop으로 넘기기
 
     return (
         <div className="section">
@@ -284,20 +280,6 @@ export default function MyPage() {
                     />
                 </div>
             </div>
-
-            {/* dDay / 커버 이미지 뺌 */}
-            {/* <div className="myPage-div-four"> */}
-            {/* <h3 className="myPage-h3">메인화면 설정</h3>
-                <SetMainList
-                    setDdayPin={setDdayPin}
-                    dDayPin={dDayPin}
-                    handleCheckDday={handleCheckDday}
-                    setDonePin={setDonePin}
-                    donePin={donePin}
-                    handleCheckDone={handleCheckDone}
-                /> */}
-            {/* <PsnCoverImg /> */}
-            {/* </div> */}
 
             <div className="myPage-div-five">
                 <div className="myPage-div-five-one">
