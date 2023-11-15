e829be78ca3e7b2030046d204b684560cd65d6bf
saywhat <95282021+loveflora@users.noreply.github.com>
2023-11-15 13:42:23 +0900

    refactor: 테스터 로그인 버튼 2개

--- client/src/pages/user/Login.tsx
+++ client/src/pages/user/Login.tsx
@@ -5,13 +5,19 @@ import '../../styles/scss/pages/user/login.scss';
 import GoogleLoginBtn from '../../components/login/GoogleLoginBtn';
 import NaverLoginBtn from '../../components/login/NaverLoginBtn';
 import KakaoLoginBtn from '../../components/login/KakaoLoginBtn';
-import TesterLoginBtn from '../../components/login/TesterLoginBtn';
+import TesterLoginBtn1 from '../../components/login/TesterLoginBtn1';
+import TesterLoginBtn2 from '../../components/login/TesterLoginBtn2';
 import { Button } from '@mui/material';
 
 export default function Login() {
-    const testLogin = (testNum: number): void => {
+    const testLogin1 = (testNum: number): void => {
         window.location.href = `${process.env.REACT_APP_DB_HOST}/user/login/test?testNum=${testNum}`;
     };
+
+    const testLogin2 = (testNum: number): void => {
+        window.location.href = `${process.env.REACT_APP_DB_HOST}/user/login/test?testNum=${testNum}`;
+    };
+
     const googleLogin = (): void => {
         window.location.href = `${process.env.REACT_APP_DB_HOST}/user/login/google`;
     };
@@ -28,13 +34,19 @@ export default function Login() {
             <div className="login-wrapper">
                 <p id="login-p">Motimates와 목표를 이뤄볼까요?</p>
 
-                <TesterLoginBtn
+                <TesterLoginBtn1
                     style={{ marginTop: '20%', width: '60%' }}
-                    onClick={() => testLogin(1)}
-                    // onClick={() => testLogin(2)}
+                    onClick={() => testLogin1(1)}
                     align="center"
                     className="loginBtn"
-                ></TesterLoginBtn>
+                ></TesterLoginBtn1>
+
+                <TesterLoginBtn2
+                    style={{ marginTop: '20%', width: '60%' }}
+                    onClick={() => testLogin2(2)}
+                    align="center"
+                    className="loginBtn"
+                ></TesterLoginBtn2>
 
                 <GoogleLoginBtn
                     style={{ marginTop: '2%', width: '60%' }}
